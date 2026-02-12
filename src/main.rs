10012afb836a55fa47289d33d97d0b547400634d
Michal Procházka <michal.prochazka@tul.cz>
2026-02-12 17:42:55 +0100

    Added glow example

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,170 @@
+use glutin::{
+	config::{ConfigTemplateBuilder, GlConfig},
+	context::{ContextApi, ContextAttributesBuilder, NotCurrentGlContext, PossiblyCurrentContext},
+	display::{GetGlDisplay, GlDisplay},
+	surface::{GlSurface, SwapInterval, Surface, WindowSurface},
+};
+use winit::{
+	application::ApplicationHandler, event::WindowEvent, event_loop::ActiveEventLoop,
+	window::WindowId,
+};
+use glutin_winit::{DisplayBuilder, GlWindow};
+use raw_window_handle::HasWindowHandle;
+use glow::*;
+
+use std::num::NonZeroU32;
+
+struct App {
+	gl: Context,
+	gl_surface: Surface<WindowSurface>,
+	gl_context: PossiblyCurrentContext,
+}
+
+impl ApplicationHandler for App {
+	fn window_event(
+		&mut self,
+		event_loop: &ActiveEventLoop,
+		_window_id: WindowId,
+		event: WindowEvent,
+	) {
+		match event {
+			WindowEvent::CloseRequested => {
+				event_loop.exit();
+			}
+			WindowEvent::RedrawRequested => unsafe {
+				self.gl.clear(glow::COLOR_BUFFER_BIT);
+				self.gl.draw_arrays(glow::TRIANGLES, 0, 3);
+				self.gl_surface.swap_buffers(&self.gl_context).unwrap();
+			},
+			_ => (),
+		}
+	}
+
+	fn resumed(&mut self, _event_loop: &winit::event_loop::ActiveEventLoop) {}
+}
+
 fn main() {
-    println!("Hello, world with GPG!");
+	unsafe {
+		let shader_version = "#version 410";
+
+		let event_loop = winit::event_loop::EventLoop::builder().build().unwrap();
+		let window_builder = winit::window::Window::default_attributes()
+			.with_title("Hello triangle!")
+			.with_inner_size(winit::dpi::LogicalSize::new(1024.0, 768.0));
+
+		let template = ConfigTemplateBuilder::new();
+
+		let display_builder =
+			DisplayBuilder::new().with_window_attributes(Some(window_builder));
+
+		let (window, gl_config) = display_builder
+			.build(&event_loop, template, |configs| {
+				configs
+					.reduce(|accum, config| {
+						if config.num_samples() > accum.num_samples() {
+							config
+						} else {
+							accum
+						}
+					})
+					.unwrap()
+			})
+			.unwrap();
+
+		let raw_window_handle = window
+			.as_ref()
+			.and_then(|window| window.window_handle().map(Into::into).ok());
+
+		let gl_display = gl_config.display();
+		let context_attributes = ContextAttributesBuilder::new()
+			.with_context_api(ContextApi::OpenGl(Some(glutin::context::Version {
+				major: 4,
+				minor: 1,
+			})))
+			.build(raw_window_handle);
+
+		let not_current_gl_context = gl_display
+			.create_context(&gl_config, &context_attributes)
+			.unwrap();
+
+		let window = window.unwrap();
+
+		let attrs = window.build_surface_attributes(Default::default()).unwrap();
+		let gl_surface = gl_display
+			.create_window_surface(&gl_config, &attrs)
+			.unwrap();
+
+		let gl_context = not_current_gl_context.make_current(&gl_surface).unwrap();
+
+		let gl = glow::Context::from_loader_function_cstr(|s| gl_display.get_proc_address(s));
+
+		gl_surface
+			.set_swap_interval(&gl_context, SwapInterval::Wait(NonZeroU32::new(1).unwrap()))
+			.unwrap();
+
+		let vertex_array = gl
+			.create_vertex_array()
+			.expect("Cannot create vertex array");
+		gl.bind_vertex_array(Some(vertex_array));
+
+		let program = gl.create_program().expect("Cannot create program");
+
+		let (vertex_shader_source, fragment_shader_source) = (
+			r#"const vec2 verts[3] = vec2[3](
+				vec2(0.5f, 1.0f),
+				vec2(0.0f, 0.0f),
+				vec2(1.0f, 0.0f)
+			);
+			out vec2 vert;
+			void main() {
+				vert = verts[gl_VertexID];
+				gl_Position = vec4(vert - 0.5, 0.0, 1.0);
+			}"#,
+			r#"precision mediump float;
+			in vec2 vert;
+			out vec4 color;
+			void main() {
+				color = vec4(vert, 0.5, 1.0);
+			}"#,
+		);
+
+		let shader_sources = [
+			(glow::VERTEX_SHADER, vertex_shader_source),
+			(glow::FRAGMENT_SHADER, fragment_shader_source),
+		];
+
+		let mut shaders = Vec::with_capacity(shader_sources.len());
+
+		for (shader_type, shader_source) in shader_sources.iter() {
+			let shader = gl
+				.create_shader(*shader_type)
+				.expect("Cannot create shader");
+			gl.shader_source(shader, &format!("{}\n{}", shader_version, shader_source));
+			gl.compile_shader(shader);
+			if !gl.get_shader_compile_status(shader) {
+				panic!("{}", gl.get_shader_info_log(shader));
+			}
+			gl.attach_shader(program, shader);
+			shaders.push(shader);
+		}
+
+		gl.link_program(program);
+		if !gl.get_program_link_status(program) {
+			panic!("{}", gl.get_program_info_log(program));
+		}
+
+		for shader in shaders {
+			gl.detach_shader(program, shader);
+			gl.delete_shader(shader);
+		}
+
+		gl.use_program(Some(program));
+		gl.clear_color(0.1, 0.2, 0.3, 1.0);
+
+		let _ = event_loop.run_app(&mut App {
+			gl,
+			gl_surface,
+			gl_context,
+		});
+	}
 }
